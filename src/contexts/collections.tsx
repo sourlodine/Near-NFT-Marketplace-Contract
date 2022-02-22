e6b258afdfa76381aaf1d47d53c30c2c35bc202a
sourlodine <harran39318@gmail.com>
2022-02-22 12:22:24 +0100

    added context for collections

---
+++ src/contexts/collections.tsx
@@ -0,0 +1,55 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react"
+import { getCollections } from "../helpers/collections"
+import { TCollection } from "../pages/CollectionPage/CollectionPage"
+import { ConnectionContext } from "./connection"
+import { ContractContext } from "./contract"
+
+interface CollectionContextProps {
+  collections: TCollection[]
+  isFetchingCollections: boolean
+  fetchCollections: Function
+}
+
+export const CollectionContext = createContext<CollectionContextProps>({
+  collections: [],
+  isFetchingCollections: true,
+  fetchCollections: () => {},
+})
+
+const CollectionProvider = (props: any) => {
+  const [collections, setCollections] = useState<TCollection[]>([])
+  const { contractAccountId } = useContext(ContractContext)
+  const { provider } = useContext(ConnectionContext)
+  const [isFetchingCollections, setIsFetchingCollections] = useState(true)
+
+  const fetchCollections = useCallback(async () => {
+    try {
+      setIsFetchingCollections(true)
+      const results = await getCollections(provider, contractAccountId)
+      console.log({ results })
+      setCollections(results)
+      setIsFetchingCollections(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchCollections()
+  }, [fetchCollections])
+  return (
+    <CollectionContext.Provider
+      value={{ collections, isFetchingCollections, fetchCollections }}
+    >
+      {props.children}
+    </CollectionContext.Provider>
+  )
+}
+
+export default CollectionProvider
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import ConnectionProvider from "./contexts/connection";
-import ContractProvider from "./contexts/contract";
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import CollectionProvider from "./contexts/collections"
+import ConnectionProvider from "./contexts/connection"
+import ContractProvider from "./contexts/contract"
 
 ReactDOM.render(
   <ConnectionProvider>
     <ContractProvider>
-      <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
+      <CollectionProvider>
+        <React.StrictMode>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </React.StrictMode>
+      </CollectionProvider>
     </ContractProvider>
   </ConnectionProvider>,
   document.getElementById("root")
-);
+)
